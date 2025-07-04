f059287cda432a9cd4558e9c4ab14a41affe2b38
Kizito Arinze Chukwu <kizitoarinze50@gmail.com>
2025-07-04 12:43:22 +0200

    Add files via upload

---
+++ Day001_BandNameGenerator/README.md
@@ -0,0 +1,11 @@
+# Day 1 – Band Name Generator 🎸
+
+## 💡 Project Description
+A simple Python script that asks the user for their city and pet's name and combines them to suggest a band name.
+
+## 🧰 Tools Used
+- Python 3
+
+## 📦 How to Run
+```bash
+python3 band_name_generator.py
---
+++ Day001_BandNameGenerator/band_name_generator.py
@@ -0,0 +1,8 @@
+#This line of code will take an input using the input() function 
+print("Hello " + input("What is your Name? ") + "!")
+print("Welcome to Kizito's Band name Generator.")
+# input("What is the name of the city you grew up in? ")
+city_name = input("What is the name of the city you grew up in?\n")
+# input("What is your pet's name? ")
+pet_name = input("What is your pet's name?\n")
+print("Your band name could be: " + city_name + " " + pet_name)
